31baef55f8383f5377d872ead02027c2d5010eef
Jeremy <jeremy.perrouault@ascadis.fr>
2025-12-11 11:08:48 +0000

    JOUR 1 - CORRECTION EXO 2 BITWISE

--- intro-java/ExoBitwiseApplication.java
+++ intro-java/ExoBitwiseApplication.java
@@ -21,5 +21,17 @@ public class ExoBitwiseApplication {
 
         System.out.println("Valeur => " + result);
         System.out.println("Valeur binaire => " + Integer.toBinaryString(result));
+
+        System.out.println("-- Solution #1 pour la relecture --");
+
+        System.out.println((result >>> 2) == 1);
+        System.out.println((result >>> 1) == 1);
+        System.out.println((1 & (result >>> 0)) == 1);
+
+        System.out.println("-- Solution #2 pour la relecture --");
+
+        System.out.println((result & (1 << 2)) != 0);
+        System.out.println((result & (1 << 1)) != 0);
+        System.out.println((result & 1) != 0);
     }
 }
