f1893f1b941027c4744aafc0bd1f9b76244eb0be
Jeremy <jeremy.perrouault@ascadis.fr>
2025-12-11 13:25:31 +0000

    JOUR 1 - RAPPELS POO

--- rappels-poo/FncApplication.java
+++ rappels-poo/FncApplication.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class FncApplication {
     public static void main(String[] args) {
@@ -22,14 +23,40 @@ public class FncApplication {
         List<String> prenoms = List.of("Jérémy", "Marion", "Albert");
 
         prenoms.stream()
+            // .filter(prenom -> prenom.contains("m") || prenom.contains("M"))
+            // .filter(prenom -> prenom.toUpperCase().contains("M"))
             .map(prenom -> prenom.toUpperCase())
+            .filter(prenom -> prenom.contains("M"))
             .forEach(prenom -> System.out.println(prenom))
         ;
+
+        System.out.println("----");
 
         prenoms.stream()
             .map(String::toUpperCase)
             .forEach(System.out::println)
         ;
+
+        List<Integer> entiers = List.of(5, 42, 741, 100, 85);
+
+        int total = 0;
+
+        // for (Integer val : entiers) {
+        //     total += val;
+        // }
+
+        // entiers.stream().forEach(val -> total += val); // KO
+
+        int result = entiers.stream()
+            .reduce(0, (tot, val) -> tot + val)
+            .intValue()
+        ;
+
+        result = entiers.stream()
+            .reduce(0, Integer::sum)
+            .intValue()
+        ;
+        System.out.println(result);
 
     }
 }
