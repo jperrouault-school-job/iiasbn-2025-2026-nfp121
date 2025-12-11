bbc29f921630f16e968a194d45983317579875bb
Jeremy <jeremy.perrouault@ascadis.fr>
2025-12-11 14:59:09 +0000

    JOUR 1 - REFERENCES

---
+++ rappels-poo/ReferenceApplication.java
@@ -0,0 +1,30 @@
+public class ReferenceApplication {
+    public static void main(String[] args) {
+        Integer a = 5;
+        Personne p = new Personne();
+
+        p.age = 5;
+        // demoRef(a);
+        demoRef(p);
+
+        // System.out.println(a);
+        System.out.println(p.age);
+    }
+
+    public static void demoRef(Integer a) {
+        a = 6;
+    }
+
+    public static void demoRef(Personne p) {
+        // p.age = 6 ;
+        new Personne();
+        // p = new Personne();
+        p.age = 6;
+
+        // return p;
+    }
+
+    public static class Personne {
+        public int age = 0;
+    }
+}
