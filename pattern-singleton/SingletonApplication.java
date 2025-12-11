fe02478533166e927db24cdc42677bd219296462
Jeremy <jeremy.perrouault@ascadis.fr>
2025-12-11 15:05:28 +0000

    JOUR 1 - DEMO INTROSPECT

--- pattern-singleton/SingletonApplication.java
+++ pattern-singleton/SingletonApplication.java
@@ -1,8 +1,18 @@
+import java.lang.reflect.Constructor;
+
 public class SingletonApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         DbContext db1 = DbContext.getInstance();
         DbContext db2 = DbContext.getInstance();
 
         System.out.println(db1 == db2);
+
+        // Démo Instrospection + Reflexivité
+        Constructor<DbContext> ctor = DbContext.class.getDeclaredConstructor();
+
+        ctor.setAccessible(true);
+        DbContext db3 = ctor.newInstance();
+
+        System.out.println(db1 == db3);
     }
 }
