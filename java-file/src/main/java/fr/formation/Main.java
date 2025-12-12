2a8da2842d98069c2cb06ecd998c7a18bff79b09
Jeremy <jeremy.perrouault@ascadis.fr>
2025-12-12 10:56:00 +0000

    JOUR 2 - INTRO FILE

---
+++ java-file/src/main/java/fr/formation/Main.java
@@ -0,0 +1,51 @@
+package fr.formation;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        // Stream pour les octets
+        // Output pour l'écriture
+        // Input pour la lecture
+        // File pour manipuler un fichier
+
+        // Un fichier, en écriture d'octets : FileOutputStream
+
+        // FileOutputStream fos = null;
+
+        // try {
+        //     fos = new FileOutputStream("lefichier.txt");
+        // }
+
+        // catch (FileNotFoundException ex) {
+        //     System.out.println("Fichier introuvable !");
+        // }
+
+        // finally {
+        //     if (fos != null) {
+        //         try {
+        //             fos.close();
+        //         }
+
+        //         catch (IOException ex) {
+        //             System.out.println("Erreur à la fermeture du fichier ...");
+        //         }
+        //     }
+        // }
+
+        try (FileOutputStream fos = new FileOutputStream("lefichier.txt")) {
+            // fos.write("Démonstration".getBytes());
+            fos.write(65);
+        }
+
+        catch (FileNotFoundException ex) {
+            System.out.println("Fichier introuvable !");
+        }
+
+        catch (IOException ex) {
+            System.out.println("Erreur à la fermeture du fichier ...");
+        }
+    }
+}
