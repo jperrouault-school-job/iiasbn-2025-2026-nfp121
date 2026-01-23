05f09130dbea9adc26b2fa0251da3402a9ac1388
Jeremy <jeremy.perrouault@gmail.com>
2026-01-23 10:04:43 +0000

    JOUR 4 - CORRECTION TEST UNIT

--- java-test-junit/src/main/java/fr/formation/Calculatrice.java
+++ java-test-junit/src/main/java/fr/formation/Calculatrice.java
@@ -4,4 +4,25 @@ public class Calculatrice {
     public int additionner(int a, int b) {
         return a + b;
     }
+
+    public int additionner(String value) {
+        if (value == null || value.isBlank()) {
+            return 0;
+        }
+
+        String[] values = value.split("[,;\\n]+");
+        int total = 0;
+
+        for (String val : values) {
+            int valInt = Integer.parseInt(val);
+
+            if (valInt < 0) {
+                throw new NegativeNotAllowedException();
+            }
+
+            total += valInt;
+        }
+
+        return total;
+    }
 }
--- java-test-junit/src/test/java/fr/formation/CalculatriceTest.java
+++ java-test-junit/src/test/java/fr/formation/CalculatriceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatriceTest {
     private Calculatrice calc;
@@ -60,5 +62,77 @@ public class CalculatriceTest {
 
         // Then
         Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldReturn0IfEmptyOrNull(String value) {
+        // Given
+
+        // When
+        int result = calc.additionner(value);
+
+        // Then
+        Assertions.assertEquals(0, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "2", "5", "6" })
+    void shouldReturnValueWhenUniqValue(String value) {
+        // Given
+        int expected = Integer.parseInt(value);
+
+        // When
+        int result = calc.additionner(value);
+
+        // Then
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "12,5,19:36",
+        "12;5;19:36",
+        "12;5,19;8,7:51",
+    }, delimiter = ':')
+    void shouldReturnCorrectValues(String value, int expected) {
+        // Given
+
+        // When
+        int result = calc.additionner(value);
+
+        // Then
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    void shouldReturn35() {
+        // Given
+        String value = """
+        12
+        5
+        18
+        """;
+
+        // When
+        int result = calc.additionner(value);
+
+        // Then
+        Assertions.assertEquals(35, result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "5,-4,6",
+        "0,1,-10"
+    })
+    void shouldThrowNegativeNotAllowedExceptionIfNegative(String value) {
+        // Given
+
+        // When & Then
+        Assertions.assertThrows(
+            NegativeNotAllowedException.class,
+            () -> calc.additionner(value)
+        );
     }
 }
