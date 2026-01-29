942ab13377584abc22299da79b00e73e32f8b66a
Jeremy <jeremy.perrouault@gmail.com>
2026-01-29 15:44:48 +0000

    JOUR 5 - IMPLEMENTATION LOGGER

--- java-web-di/src/main/java/fr/formation/Main.java
+++ java-web-di/src/main/java/fr/formation/Main.java
@@ -1,9 +1,19 @@
 package fr.formation;
 
-import fr.formation.http.HttpServer;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
 
+import fr.formation.http.HttpServer;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class Main {
     public static void main(String[] args) {
+        // Pour configurer les loggers
+        Configurator.setRootLevel(Level.DEBUG);
+
+        log.error("Le message à débug : {}, {}", "var1", "var2");
+
         ApplicationContext ctx = new WebApplicationContext(Main.class.getPackageName());
 
         new Thread(() -> {
